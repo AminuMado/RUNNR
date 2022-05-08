ca3730662e1879368593fca1fa97ad684430b1fc
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-05-08 15:01:08 +0100

    import Login and create token state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import "./App.css";
+import { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Dashboard from "./components/Dashboard/Dashboard";
 import Preferences from "./components/Preferences/Preferences";
+import Login from "./components/Login/Login";
 function App() {
+  const [token, setToken] = useState();
+  if (!token) {
+    return <Login setToken={setToken} />;
+  }
   return (
     <div className="App">
       <div className="wrapper">
