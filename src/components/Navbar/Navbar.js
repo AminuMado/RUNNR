869d146e95abbecd71f9d9a4992aa9723e140d2f
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-07-23 22:17:49 +0100

    initial commit

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import "./Navbar.css";
+
+export const Navbar = (props) => {
+  const [active, setActive] = useState(props.items[0]);
+  const navItems = props.items.map((item) => {
+    return (
+      <li
+        key={item}
+        onClick={() => setActive(item)}
+        className={active === item ? "active" : ""}
+      >
+        {item}
+      </li>
+    );
+  });
+
+  return (
+    <nav className="marketplace_primary_nav">
+      <ul>{navItems}</ul>
+    </nav>
+  );
+};
+
+function add(a, b) {
+  return a + b;
+}
+add(1, 2); // 3
