45077a12fcec6bbfa754ccf7bfcc645dd51c1278
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-07-24 15:43:03 +0100

    clean up unused code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import "./App.css";
-import { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Dashboard from "./components/Dashboard/Dashboard";
 import Preferences from "./components/Preferences/Preferences";
